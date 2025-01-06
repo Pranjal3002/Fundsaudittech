35a2be220c9b5e98d9885ca2e14b8e73fb641bdb
Pranjal <163888710+Pranjal3002@users.noreply.github.com>
2025-01-06 09:38:28 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -4,8 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
 
